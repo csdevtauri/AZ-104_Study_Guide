934d929fba9e5991f8327e24f7dfc7445b3935c2
csdevtauri <csa.devomax@gmail.com>
2024-04-10 14:31:02 +0200

    add html remplace md

--- assets/js/checkboxes.js
+++ assets/js/checkboxes.js
@@ -1,4 +1,15 @@
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-checkboxes.forEach(checkbox => {
-  checkbox.removeAttribute('disabled');
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll('.quiz-form').forEach(form => {
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      let score = 0;
+      form.querySelectorAll('.question').forEach(question => {
+        const answer = question.querySelector("input[type='checkbox']:checked");
+        if (answer && answer.classList.contains('correct')) {
+          score++;
+        }
+      });
+      alert(`Votre score est de ${score}/${form.querySelectorAll('.question').length}.`);
+    });
+  });
 });
